3aa506aea6696e852d00d6cd3ee0cdd4ae528ecf
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 14:51:31 -0400

    Implement test for valid attributes. Not passing, init method needs to be implemented

--- spec/repackaging_order_spec.rb
+++ spec/repackaging_order_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe RepackagingOrder do
-  it "is valid with a base_price and a type"
+  it "is valid with a base_price and a type" do
+    @repackaging_order = RepackagingOrder.new("1299.99", :food)
+    expect(@repackaging_order).not_to raise_error 
+  end
+  
   it "is invalid without a type"
   it "is invalid without a base_price"
 end
