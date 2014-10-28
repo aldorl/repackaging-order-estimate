61f2182b6e44cd1833c182418daadca8e5f09e9f
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 15:41:14 -0400

    Implement tests for required_employees_quanity, both valid and invalid params. Not passing, need to modify init method to account for invalid argument

--- spec/repackaging_order_spec.rb
+++ spec/repackaging_order_spec.rb
@@ -15,10 +15,17 @@ describe RepackagingOrder do
   
   # Every project has to have at least one person working on them by default
   it "returns a required_employees_quantity of 1 when not given this parameter" do
-    @repackaging_order = RepackagingOrder.new("1299.99", :food)
-    expect(@repackaging_order.required_employees_quantity).to eq(1)
+    repackaging_order = RepackagingOrder.new("1299.99", :food)
+    expect(repackaging_order.required_employees_quantity).to eq(1)
   end
   
-  it "returns a required_employees_quantity equal to the given required_employees_quantity parameter"
-  it "is invalid with a required_employees_quantity parameter of less than 1"
+  it "returns a required_employees_quantity equal to the given required_employees_quantity parameter" do
+    repackaging_order = RepackagingOrder.new("1299.99", :food, 3)
+    expect(repackaging_order.required_employees_quantity).to eq(3)
+  end
+  
+  it "is invalid with a required_employees_quantity parameter of less than 1" do
+    repackaging_order = RepackagingOrder.new("1299.99", :food, -100)
+    expect(repackaging_order.required_employees_quantity).to raise_error
+  end
 end
