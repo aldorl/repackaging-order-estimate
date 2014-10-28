36e927fb7be60337ddf74119da494eea7dc46132
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 15:35:09 -0400

    Modify init method to account for required_employees_quantity and have access to base_price, type, and required_employees_quantity

--- repackaging_order.rb
+++ repackaging_order.rb
@@ -1,9 +1,12 @@
 class RepackagingOrder
-  def initialize(base_price, type)
+  attr_accessor :base_price, :type, :required_employees_quantity
+  
+  def initialize(base_price, type, required_employees_quantity = 1)
     #Instance variables
     raise "Must have base_price" if base_price.nil?
     raise "Must have type" if type.nil?
     @base_price = base_price
     @type = type
+    @required_employees_quantity = required_employees_quantity
   end
 end
