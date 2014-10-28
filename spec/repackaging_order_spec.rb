52ba53f2ddafe3c06cd439da33e418623ae70d26
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 18:30:49 -0400

    Refactor tests to fit new tests to be implemented in a better way

--- spec/repackaging_order_spec.rb
+++ spec/repackaging_order_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe RepackagingOrder do
+  before :all do
+    @food_order = RepackagingOrder.new(1299.99, "food", 3)
+    @drugs_order = RepackagingOrder.new(5432.00, "drugs")
+    @books_order = RepackagingOrder.new(12456.95, "books", 4)
+    @electronics_order = RepackagingOrder.new(1299.99, "electronics", 1)
+  end
+  
   it "is valid with a base_price and a type" do
     expect{RepackagingOrder.new(1299.99, "food")}.not_to raise_error
     expect{RepackagingOrder.new(5432, "drugs")}.not_to raise_error
@@ -16,14 +23,12 @@ describe RepackagingOrder do
   
   # Every project has to have at least one person working on them by default
   it "returns a required_employees_quantity of 1 when not given this parameter" do
-    repackaging_order = RepackagingOrder.new(1299.99, "food")
-    expect(repackaging_order.required_employees_quantity).to eq(1)
+    expect(@drugs_order.required_employees_quantity).to eq(1)
   end
   
   # Same as it "is valid with a base_price, a type and a required_employees_quantity"
   it "returns a required_employees_quantity equal to the given required_employees_quantity parameter" do
-    repackaging_order = RepackagingOrder.new(1299.99, "food", 3)
-    expect(repackaging_order.required_employees_quantity).to eq(3)
+    expect(@food_order.required_employees_quantity).to eq(3)
   end
   
   it "is invalid with a required_employees_quantity parameter of less than 1" do
@@ -51,20 +56,13 @@ describe RepackagingOrder do
   end
   
   
-  describe "modify required_employees_quantity"
-     it "adds one employee to the required_employees_quantity"
-     it "changes the required_employees_quantity to the given new_required_employees_quantity parameter"
-   end
+  describe "modify required_employees_quantity" do
+    it "adds one employee to the required_employees_quantity"
+    it "changes the required_employees_quantity to the given new_required_employees_quantity parameter"
+  end
   
   
   describe "calculates repackaging order estimate" do
-    before :all do
-      @food_order = RepackagingOrder.new(1299.99, "food", 3)
-      @drugs_order = RepackagingOrder.new(5432.00, "drugs")
-      @books_order = RepackagingOrder.new(12456.95, "books", 4)
-      @electronics_order = RepackagingOrder.new(1299.99, "electronics", 1)
-    end
-    
     context "with extra markup on top of flat markup" do
       it "adds only an extra markup of 1.2% per required_employees_quantity if the repackaging order is not of type 'food', 'drugs' or 'electronics'" do
         expect(@books_order.final_cost_estimate).to eq(13707.63) #(12456.95 *1.05 round(2)) *(1 + 0.012*4) round(2)
