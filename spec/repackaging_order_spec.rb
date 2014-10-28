c8bccc5c67cba49f61971d95400f530483439fa2
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 14:44:56 -0400

    Add pending validation tests for repackaging_order class create method

--- spec/repackaging_order_spec.rb
+++ spec/repackaging_order_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe RepackagingOrder do
-  
+  it "is valid with a base_price and a type"
+  it "is invalid without a type"
+  it "is invalid without a base_price"
 end
