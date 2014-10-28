8c5117e353dcffdf6f6eed5306a07ba2f7816f7f
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 15:03:46 -0400

    Implement tests for invalid attributes. Not passing, init method needs to be modified to account for this

--- spec/repackaging_order_spec.rb
+++ spec/repackaging_order_spec.rb
@@ -5,6 +5,11 @@ describe RepackagingOrder do
     expect{RepackagingOrder.new("1299.99", :food)}.not_to raise_error 
   end
   
-  it "is invalid without a type"
-  it "is invalid without a base_price"
+  it "is invalid without a type" do
+    expect{RepackagingOrder.new("1299.99", :nil)}.to raise_error 
+  end
+  
+  it "is invalid without a base_price" do
+    expect{RepackagingOrder.new(:nil, :food)}.to raise_error 
+  end
 end
