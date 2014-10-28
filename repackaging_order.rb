e0cfbc3b9de4922803465ac36322287d3f522bc2
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-10-28 16:51:49 -0400

    Refactor class to get rid of unnecessary error raise lines

--- repackaging_order.rb
+++ repackaging_order.rb
@@ -3,13 +3,9 @@ class RepackagingOrder
   
   def initialize(base_price, type, required_employees_quantity = 1)
     #Instance variables
-    raise "Must have base_price" if base_price.nil?
-    raise "base_price must be a numeric value" unless base_price.is_a?(Numeric)
-    raise "base_price cannot be equal or less than 0" if base_price <= 0
-    raise "Must have type" if type.nil?
-    raise "type must be a string value" unless type.is_a?(String)
-    raise "required_employees_quantity must be an integer value" unless required_employees_quantity.is_a?(Integer)
-    raise "Must be equal or greater than 1" if required_employees_quantity < 1
+    raise "Must have a numeric base_price value greater than 0" if base_price <= 0
+    raise "Must have a type parameter given as a string" unless type.is_a?(String)
+    raise "Must have an integer required_employees_quantity value equal or greater than 1" if !required_employees_quantity.is_a?(Integer) || required_employees_quantity < 1
     @base_price = base_price
     @type = type
     @required_employees_quantity = required_employees_quantity
